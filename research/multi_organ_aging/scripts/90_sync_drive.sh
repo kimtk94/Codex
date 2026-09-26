ba24bd47e6ba539f822d0272d2d0946d2f5235d6
Kim TaeHoon <65493390+kimtk94@users.noreply.github.com>
2026-09-27 01:29:18 +0900

    Complete multi-organ aging pipeline

--- research/multi_organ_aging/scripts/90_sync_drive.sh
+++ research/multi_organ_aging/scripts/90_sync_drive.sh
@@ -8,19 +8,22 @@ PROJECT="/srv/is-analysis/IS_Analysis_V3/research/multi_organ_aging"
 RESULTS="/srv/is-analysis/results/multi_organ_aging"
 REMOTE="gdrive:IS_Analysis_V3/MULTI_ORGAN_AGING"
 
-echo "===== SYNC CODE ====="
-rclone copy "$PROJECT" "$REMOTE/code"   --exclude '.git/**'   --exclude '__pycache__/**'   --exclude '*.pyc'   --progress
+echo "===== SYNC MASTER / CODE ====="
+rclone copyto "$PROJECT/MULTI_ORGAN_AGING_MASTER.md" "$REMOTE/00_MASTER/MULTI_ORGAN_AGING_MASTER.md" --progress
+RC0=$?
+
+rclone copy "$PROJECT" "$REMOTE/02_CODE"   --exclude '.git/**'   --exclude '__pycache__/**'   --exclude '*.pyc'   --progress
 RC1=$?
 
 echo
 echo "===== SYNC RESULTS ====="
-rclone copy "$RESULTS" "$REMOTE/results"   --exclude '*.joblib'   --exclude '*.pkl'   --progress
+rclone copy "$RESULTS" "$REMOTE/03_RESULTS"   --exclude '*.joblib'   --exclude '*.pkl'   --progress
 RC2=$?
 
 echo
-echo "code_rc=$RC1 results_rc=$RC2"
+echo "master_rc=$RC0 code_rc=$RC1 results_rc=$RC2"
 
-if [ "$RC1" -ne 0 ] || [ "$RC2" -ne 0 ]; then
+if [ "$RC0" -ne 0 ] || [ "$RC1" -ne 0 ] || [ "$RC2" -ne 0 ]; then
   exit 1
 fi
 
